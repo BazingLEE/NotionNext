e00d0c634c9757089bc784471861971fafb7742f
芝奕 <52346948+BazingLEE@users.noreply.github.com>
2023-03-13 00:13:23 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['Angi是继Vamei之后最接近赤道的台风', '按照气象规律，台风不常出现在赤道', '所以，Angi是离群的风，无所顾忌地生长，不着边际地游荡'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
 
